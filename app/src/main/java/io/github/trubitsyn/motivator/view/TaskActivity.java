aaed755c29416d7b04f320ff7ffb964290cab3fa
Nikola Trubitsyn <nikola.trubitsyn@gmail.com>
2018-05-31 00:47:57 +0300

    Add GPLv3 headers

--- app/src/main/java/io/github/trubitsyn/motivator/view/TaskActivity.java
+++ app/src/main/java/io/github/trubitsyn/motivator/view/TaskActivity.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2016 Nikola Trubitsyn
+ *
+ * This file is part of Motivator.
+ *
+ * Motivator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Motivator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Motivator.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.trubitsyn.motivator.view;
 
 import android.content.Intent;
